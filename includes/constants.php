4dcde500e29ed987c254ef354b6650c5b4a10aa0
Florian Engelhardt <florian0engelhardt@gmail.com>
2026-05-14 00:28:00 +0200

    fix: use existing install twig templates in upgrade flow

--- includes/constants.php
+++ includes/constants.php
@@ -56,6 +56,9 @@ if(!defined('CACHE_PATH')) {
 
 define('COMBAT_ENGINE'				, 'xnova');
 
+// Required database schema version used by install/upgrade checks.
+define('DB_VERSION_REQUIRED'      , 12);
+
 // For Fatal Errors!
 define('DEFAULT_LANG'				, 'de');
 
