6dbac77c94a7c110c6a9fd98676069a381aadfde
Florian Engelhardt <florian0engelhardt@gmail.com>
2026-05-14 00:44:21 +0200

    fix: remove unsupported install twig namespace includes

--- install/index.php
+++ install/index.php
@@ -29,6 +29,12 @@ $LNG->includeData(array('L18N', 'INGAME', 'INSTALL', 'CUSTOM'));
 
 $mode = HTTP::_GP('mode', '');
 
+if (!defined('DB_PREFIX') && file_exists('includes/config.php') && filesize('includes/config.php') !== 0) {
+	$databaseConfig = array();
+	require 'includes/config.php';
+	define('DB_PREFIX', (string)($databaseConfig['prefix'] ?? ''));
+}
+
 $template = new template();
 // Twig caching is handled automatically in class.template.php
 $template->assign_vars(array(
@@ -139,7 +145,7 @@ switch ($mode) {
 			'header'        => $LNG['menu_upgrade']
 		));
 
-		$template->show('ins_update.tpl');
+		$template->show('ins_upgrade.twig');
 		break;
 	case 'doupgrade':
 		// TODO:Need a rewrite!
@@ -257,7 +263,7 @@ switch ($mode) {
 			'revision' => $revision,
 			'header'   => $LNG['menu_upgrade'],
         ));
-		$template->show('ins_doupdate.tpl');
+		$template->show('ins_doupdate.twig');
         unlink($enableInstallToolFile);
 		break;
 	case 'install':
